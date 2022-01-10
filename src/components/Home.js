ec809cc5a0151df7b42db6ff8de5009701e23606
JazzPants <leighton.l14@gmail.com>
2022-01-10 19:12:31 +1100

    Done!

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 const Home = () => {
   return (
     <div>
       {/*{code here}*/}
+      <h1>Home Page</h1>
     </div>
   );
 };
+
+// ReactDOM.render(
+//   <Router>
+//     <Route path="/" component={Home} />
+//     </Router>,
+//      document.getElementById("root"));
 
 export default Home;
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,9 +4,18 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+      {movies.map(movie => <div>
+        <h1>{movie.title}</h1>
+      <p>Time: {movie.time}</p>
+      <p>Genres:</p>
+      <ul>{movie.genres.map(genre => <li>{genre}</li>)}</ul>
+       </div>
+      )}
     </div>
-  );
-};
+  )
+}
+
+
 
 export default Movies;
